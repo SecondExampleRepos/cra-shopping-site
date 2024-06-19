2c773e84c9447ef566c339584c8530b6329e6a12
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-19 19:28:12 +0000

    edit_file
    # Path: step_6/for_each/edit_file
    # UUID: f93f5cbe-b951-4404-adbc-a4de1f90694c
    
    # Changed files:
    postcss.config.js
    - 75fa27015021542936fd7d90e4f921966c67885c

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+// Before
+import logo from '/logo.png'
+
+// After
+import logo from '../public/logo.png'
+
+// Before
+<img src={logo} />
+
+// After
+<img src={logo.src} />
+
 module.exports = {
   plugins: {
     tailwindcss: {},
