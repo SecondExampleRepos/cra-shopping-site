374e6f0085521a0a8d450ce9cee0aae2cfc0907d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-19 19:27:59 +0000

    create_file_with_ai
    # Path: step_3/create_file_with_ai
    # UUID: 9c32615b-81e5-48ff-9aa9-b615378b996f
    
    # Changed files:
    app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import 'src/components/Product/Product.css'
+import 'src/components/ProductList/ProductList.css'
+
+export const metadata: Metadata = {
+  title: 'React Shop',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  )
+}
